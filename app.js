6c24ece356e4cdff6c210adccc2660c57bbd207a
amit-gshe <scybhe@163.com>
2018-08-28 22:59:30 +0800

    disable x-powered-by

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.disable('x-powered-by');
+// app.disable('etag')
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
